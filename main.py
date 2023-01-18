b881b49a937a51e630f95f5a4ff330518de3f0f6
Mishutka <68320483+misha720@users.noreply.github.com>
2023-01-18 06:42:33 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -20,20 +20,32 @@ from kivy.uix.widget import Widget
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
+from kivy.uix.recycleview import RecycleView
 
 Window.title = "Every Day"
 Window.clearcolor = (0/255, 0/255, 0/255)
 
-class View(BoxLayout):
+class PlansScreen(Screen):
+	def __init__(self, **kwargs):
+		super(PlansScreen,self).__init__(**kwargs)
+		self.rv.data = [{'text': str(x)} for x in range(100)]
+
+class TodayScreen(Screen):
 	pass
 
 class EveryDayApp(App):
-	def __init__(self, ):
-		super(EveryDayApp,self).__init__()
+	def __init__(self, **kwargs):
+		super(EveryDayApp,self).__init__(**kwargs)
 		self.load_kv("main.kv")
 
 	def build(self):
-		return View()
+		self.sm = ScreenManager(transition=NoTransition())
+
+		self.sm.add_widget(TodayScreen(name="today"))
+		self.sm.add_widget(PlansScreen(name="plans"))
+
+		return self.sm
 
 if __name__ == '__main__':
 	EveryDayApp().run()
