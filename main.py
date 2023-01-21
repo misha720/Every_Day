7924fe0b33fff85e37024f7eab0d2dfd003d52fc
Mishutka <68320483+misha720@users.noreply.github.com>
2023-01-21 16:32:28 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,21 @@
 	Графическое окружение
 '''
 
+''' 
+	COLORS:
+		(0/255, 0/255, 0/255, 1) - Background
+		(63/255, 63/255, 63/255, 1) - Ponel
+
+		(135/255, 194/255, 133/255, 1) - Green
+		(153/255, 219/255, 150/255, 1) - Duble Green
+
+		(135/255, 194/255, 133/255, 1) - Red
+
+		(79/255, 56/255, 146/255, 1) - Purple
+		(108/255, 73/255, 211/255, 1) - Duble Purple
+		(183/255, 162/255, 245/255, 1) - TextInput Purple
+'''
+
 #	KIVY
 from kivy.config import Config
 Config.set('graphics', 'resizable', 0)
@@ -30,9 +45,42 @@ class PlansScreen(Screen):
 	def __init__(self, **kwargs):
 		super(PlansScreen,self).__init__(**kwargs)
 
-		self.rv.data = [{'text': "Тихо покакать, что бы миша не услышал"},
-		{'text': "Купить мише жижу"},
-		{'text': 'Предъявить ему за то, что он мне давно не писал "я тебя лублу"'}]
+		self.ctrl_add_tast = 0
+		self.input_add_task.opacity = 0
+		self.input_add_task.size = (0,0)
+
+		self.rv.data = [{'text': "Купить мише жижу"}]
+
+	def add_new_task(self):
+		if self.ctrl_add_tast == 0:
+			self.add_task.text = "Отмена"
+			self.add_task.background_color = (219/255, 74/255, 74/255)
+			self.input_add_task.opacity = 1
+			self.input_add_task.size = (395,175)
+
+			self.ctrl_add_tast = 1
+
+		elif self.ctrl_add_tast == 1:
+			self.add_task.text = "Добавить"
+			self.add_task.background_color = (135/255, 194/255, 133/255)
+			self.input_add_task.opacity = 0
+			self.input_add_task.size = (0,0)
+
+			self.ctrl_add_tast = 0
+
+	def add_input_task(self):
+		title = self.add_input_title_task.text
+		keys = self.add_input_keys_task.text
+
+		self.rv.data.append( {'text':title} )
+
+		self.add_input_title_task.text = ""
+		self.add_input_keys_task.text = ""
+
+		self.add_task.text = "Добавить"
+		self.add_task.background_color = (135/255, 194/255, 133/255)
+		self.input_add_task.opacity = 0
+		self.input_add_task.size = (0,0)
 
 class TodayScreen(Screen):
 	pass
